ab28a186ea1b7121444e7721cec919d03ede41e1
Tatiana Antonova <to.antonova@mail.ru>
2022-12-14 17:19:27 +0700

    fix Engine.php (add empty lines)

--- src/Engine.php
+++ src/Engine.php
@@ -13,16 +13,20 @@ function startGame(string $taskText, callable $questionAndRightAnswer)
     $name = prompt('May I have your name?');
     line("Hello, %s!", $name);
     line($taskText);
+
     for ($i = 0; $i < MAX_ATTEMPT; $i++) {
         [$question, $rightAnswer] = $questionAndRightAnswer();
         line('Question: %s', $question);
         $answer = prompt('Your answer');
+
         if ($rightAnswer !== $answer) {
             line("'{$answer}' is wrong answer ;(. Correct answer was '{$rightAnswer}'.");
             line("Let's try again, %s!", $name);
             return;
         }
+
         line("Correct!");
     }
+
     line('Congratulations, %s!', $name);
 }
